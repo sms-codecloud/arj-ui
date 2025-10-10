eef4e27de3b33fbd0417a61626144a3f9533f925
mahantesh <mantu.bhavi@gmail.com>
2025-10-10 13:45:10 +0530

    created components, api consumption and implemented routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { UserProvider } from "./context/userContext.jsx";
+import { ModalProvider } from "./context/modalContext.jsx";
+import Layout from "./components/Layout";
+import UserDetails from "./components/UserDetails";
+import UsersList from "./components/UserList";
+import UserEdit from "./components/UserEdit";
+import Error from "./components/Error";
+import AddUser from "./components/AddUser";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <Error />,
+      children: [
+        { path: "/", element: <UsersList /> }, // /users
+        { path: "users/:id", element: <UserDetails /> }, // /user details
+        { path: "users/:id/edit", element: <UserEdit /> }, // /user eidt
+        { path: "add-user", element: <AddUser /> }, // /user creation
+      ],
+    },
+  ]);
 
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <UserProvider>
+        <ModalProvider>
+          <RouterProvider router={appRouter} />
+        </ModalProvider>
+      </UserProvider>
     </>
   );
 }
